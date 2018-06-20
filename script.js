1595f75951a472e754877b89546a35e385720b7a
rahul <officialrahulmandal@gmail.com>
2018-06-20 18:27:40 +0530

    Stop the timer when the test is done

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0] ;
-
+var interval;
+var timerRunning = false
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time){
 if (time <= 9){
@@ -29,6 +30,7 @@ function spellCheck(){
   let textEntered = testArea.value;
   let originTextMatch = originText.substring(0,textEntered.length)
   if(textEntered == originText){
+    clearInterval(interval);
     testWrapper.style.borderColor = "#429890";
   } else {
     if(textEntered == originTextMatch){
@@ -43,8 +45,9 @@ function spellCheck(){
 // Start the timer:
 function start(){
   let textEnteredLength = testArea.value.length;
-  if (textEnteredLength==0){
-    setInterval(runTimer, 10);
+  if (textEnteredLength==0 && !timerRunning){
+    timerRunning = true;
+    interval = setInterval(runTimer, 10);
   }
 }
 
